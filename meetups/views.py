0659f02dd3e880f1bb6f603bce9bce6bdc3efd8f
Huvinesh Rajendran <81321926+Huvinesh-Rajendran-12@users.noreply.github.com>
2021-08-17 13:35:14 +0800

    second

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Hello World !')
+    meetups = [
+        {'title': 'A First Meetup',
+         'location': 'New York',
+         'slug':'a-first-meetup'},
+        {'title': 'A Second Meetup',
+         'location':'Paris',
+         'slug': 'a-second-meetup'}
+        
+    ]
+    return render(request,'meetups/welcome.html',{
+        'show_meetups':True,
+        'meetups':meetups
+    })
+
+def meetup_details(request,meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {'title': 'A First Meetup',
+                       'description':'This is the first Meetup'}
+    return render(request,'meetups/detail.html',{
+        'meetup_title':selected_meetup['title'],
+        'meetup_details':selected_meetup['description']
+    })
