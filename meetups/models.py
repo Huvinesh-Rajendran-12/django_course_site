3634883eebb97e1b6340ac383876ba37abb0158c
Huvinesh Rajendran <81321926+Huvinesh-Rajendran-12@users.noreply.github.com>
2021-08-18 11:30:47 +0800

    four

--- meetups/models.py
+++ meetups/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Meetup(models.Model):
-    title = models.TextField()
-    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
