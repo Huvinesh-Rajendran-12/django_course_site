1f428bc8a43105016430f38d32dbc002ca77e2ff
Huvinesh Rajendran <81321926+Huvinesh-Rajendran-12@users.noreply.github.com>
2021-08-19 13:58:43 +0800

    final

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import Meetup , Location, participant
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
-    list_display = ('title','slug')
-    list_filter = ('title',)
-    prepropulated_fields = {'slug':('title',)}
+    list_display = ('title','slug','location')
+    list_filter = ('location','date')
+    prepopulated_fields = {'slug':('title',)}
 admin.site.register(Meetup,MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(participant)
--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from meetups.forms import RegistrationForm
+from django.shortcuts import render, redirect 
 
-from .models import Meetup
+from .models import Meetup , participant
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -13,11 +15,28 @@ def welcome(request):
 def meetup_details(request,meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registrationForm = RegistrationForm
+           
+        else:
+            registrationForm = RegistrationForm(request.POST)
+            if registrationForm.is_valid():
+                usr_email = registrationForm.cleaned_data['email']
+                Participant, _ = participant.objects.get_or_create(email=usr_email)
+                selected_meetup.participants.add(Participant)
+                return redirect('confirm-registration',meetup_slug=meetup_slug)
+                
         return render(request,'meetups/detail.html',{
-            'meetup_found':True,
-            'meetup_title':selected_meetup.title,
-            'meetup_details':selected_meetup.description
-        })
+                'meetup_found':True,
+                'meetup':selected_meetup,
+                'form': registrationForm
+            })
     except Exception as exc:
         return render(request, 'meetups/detail.html',
                       {'meetup_found':False})
+
+def confirm_registration(request,meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request,'meetups/registration-success.html',{
+        'organizer_email': meetup.organizer_email
+    })
